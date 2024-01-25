19ed52d3defde7a7fb8f57996a27b58a784e676c
Gustavo Souza Carneiro <82236510+gscproseg@users.noreply.github.com>
2024-01-25 00:11:35 -0300

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -1,47 +1,135 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
 import cv2
+import torch
 import numpy as np
-import onnxruntime
+import tempfile
+import time
+from collections import Counter
+import json
+import pandas as pd
+from model_utils import get_yolo, color_picker_fn, get_system_stat
 
-# Carregue o modelo ONNX
-onnx_model_path = './models/best.onnx'  # Substitua pelo caminho real do seu arquivo ONNX
-onnx_session = onnxruntime.InferenceSession(onnx_model_path)
 
-# Defina uma função de retorno de chamada para processar os frames de vídeo
-def video_frame_callback(frame):
-    try:
-        if frame.type == 'video':
-            img = frame.to_ndarray(format="bgr24")
+st.sidebar.title('Settings')
+st.title('YOLOv5 Predictions')
+sample_img = cv2.imread('logo.jpg')
+FRAME_WINDOW = st.image(sample_img, channels='BGR')
+cap = None
+p_time = 0
 
-            # Pré-processamento da imagem conforme necessário
-            input_img = cv2.resize(img, (640, 640))
-            input_img = input_img.transpose((2, 0, 1)).astype(np.float32)
-            input_img = np.expand_dims(input_img, axis=0)
+path_model_file = st.sidebar.text_input(
+    'path to YOLOv5 Model:',
+    'eg: dir/YOLOv5.pt'
+)
+if st.sidebar.checkbox('Load Model'):
+    # YOLOv5 Model
+    model = torch.hub.load('.', 'custom', path=path_model_file, source='local', force_reload=True)
 
-            # Realize a inferência usando ONNX Runtime
-            pred = onnx_session.run(None, {'input': input_img})
+    # Load Class names
+    class_labels = model.names
 
-            # Pós-processamento e desenho das bounding boxes (ajuste conforme necessário)
-            # Exemplo: Desenhar um retângulo na posição (100, 100) com largura 200 e altura 100
-            cv2.rectangle(img, (100, 100), (300, 200), (0, 255, 0), 2)
+    # Inference Mode
+    options = st.sidebar.radio(
+        'Options:', ('Webcam', 'Image', 'Video', 'RTSP'), index=1)
 
-            # Retorne o frame processado
-            return img  # Substitua pela imagem processada
-    except Exception as e:
-        st.error(f"Erro no callback de vídeo: {str(e)}")
-        return None  # Retorna None para evitar quebras, mas isso pode precisar ser ajustado
+    # Confidence
+    confidence = st.sidebar.slider(
+        'Detection Confidence', min_value=0.0, max_value=1.0, value=0.25)
 
-# Use o Streamlit Webrtc para exibir a webcam e processar os frames
-def app():
-    st.title("YOLOv5 Object Detection com Streamlit")
+    # Draw thickness
+    draw_thick = st.sidebar.slider(
+        'Draw Thickness:', min_value=1,
+        max_value=20, value=3
+    )
+    
+    color_pick_list = []
+    for i in range(len(class_labels)):
+        classname = class_labels[i]
+        color = color_picker_fn(classname, i)
+        color_pick_list.append(color)
 
-    webrtc_streamer(key="example", 
-                    video_frame_callback=video_frame_callback,
-                    media_stream_constraints={"video": True, "audio": False})
+    # Image
+    if options == 'Image':
+        upload_img_file = st.sidebar.file_uploader(
+            'Upload Image', type=['jpg', 'jpeg', 'png'])
+        if upload_img_file is not None:
+            pred = st.checkbox('Predict Using YOLOv5')
+            file_bytes = np.asarray(
+                bytearray(upload_img_file.read()), dtype=np.uint8)
+            img = cv2.imdecode(file_bytes, 1)
+            FRAME_WINDOW.image(img, channels='BGR')
 
-if __name__ == "__main__":
-    try:
-        app()
-    except Exception as e:
-        st.error
+            if pred:
+                img, current_no_class = get_yolo(img, model, confidence, color_pick_list, draw_thick)
+                FRAME_WINDOW.image(img, channels='BGR')
+
+                # Current number of classes
+                class_fq = dict(Counter(i for sub in current_no_class for i in set(sub)))
+                class_fq = json.dumps(class_fq, indent = 4)
+                class_fq = json.loads(class_fq)
+                df_fq = pd.DataFrame(class_fq.items(), columns=['Class', 'Number'])
+                
+                # Updating Inference results
+                with st.container():
+                    st.markdown("<h2>Inference Statistics</h2>", unsafe_allow_html=True)
+                    st.markdown("<h3>Detected objects in curret Frame</h3>", unsafe_allow_html=True)
+                    st.dataframe(df_fq, use_container_width=True)
+    
+    # Video
+    if options == 'Video':
+        upload_video_file = st.sidebar.file_uploader(
+            'Upload Video', type=['mp4', 'avi', 'mkv'])
+        if upload_video_file is not None:
+            pred = st.checkbox('Predict Using YOLOv5')
+
+            tfile = tempfile.NamedTemporaryFile(delete=False)
+            tfile.write(upload_video_file.read())
+            cap = cv2.VideoCapture(tfile.name)
+
+    # Web-cam
+    if options == 'Webcam':
+        cam_options = st.sidebar.selectbox('Webcam Channel',
+                                        ('Select Channel', '0', '1', '2', '3'))
+    
+        if not cam_options == 'Select Channel':
+            pred = st.checkbox('Predict Using YOLOv5')
+            cap = cv2.VideoCapture(int(cam_options))
+
+    # RTSP
+    if options == 'RTSP':
+        rtsp_url = st.sidebar.text_input(
+            'RTSP URL:',
+            'eg: rtsp://admin:name6666@198.162.1.58/cam/realmonitor?channel=0&subtype=0'
+        )
+        pred = st.checkbox('Predict Using YOLOv5')
+        cap = cv2.VideoCapture(rtsp_url)
+
+if (cap != None) and pred:
+    stframe1 = st.empty()
+    stframe2 = st.empty()
+    stframe3 = st.empty()
+    while True:
+        success, img = cap.read()
+        if not success:
+            st.error(
+                f"{options} NOT working\nCheck {options} properly!!",
+                icon="🚨"
+            )
+            break
+
+        img, current_no_class = get_yolo(img, model, confidence, color_pick_list, draw_thick)
+        FRAME_WINDOW.image(img, channels='BGR')
+
+        # FPS
+        c_time = time.time()
+        fps = 1 / (c_time - p_time)
+        p_time = c_time
+        
+        # Current number of classes
+        class_fq = dict(Counter(i for sub in current_no_class for i in set(sub)))
+        class_fq = json.dumps(class_fq, indent = 4)
+        class_fq = json.loads(class_fq)
+        df_fq = pd.DataFrame(class_fq.items(), columns=['Class', 'Number'])
+        
+        # Updating Inference results
+        get_system_stat(stframe1, stframe2, stframe3, fps, df_fq)
